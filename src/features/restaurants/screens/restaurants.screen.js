94594e030a12e194e2c21ffab1401adf5b8fdd70
abhishah02 <abhishah578@gmail.com>
2021-10-23 11:14:20 +0530

    origin

---
+++ src/features/restaurants/screens/restaurants.screen.js
@@ -0,0 +1,34 @@
+import { Searchbar } from "react-native-paper";
+import React from "react";
+import { StatusBar, SafeAreaView } from "react-native";
+
+import styled from "styled-components/native";
+
+import { RestaurantInfoCard } from "../components/restaurants-info-card-component";
+
+const SafeArea = styled(SafeAreaView)`
+  flex: 1;
+  ${StatusBar.currentHeight && `margin-top: ${StatusBar.currentHeight}px`};
+`;
+
+const SearchContainer = styled.View`
+  padding: ${(props) => props.theme.space[3]};
+`;
+
+const RestaurantListContainer = styled.View`
+  flex: 1;
+  padding: ${(props) => props.theme.space[3]};
+  background-color: ${(props) => props.theme.colors.brand.primary};
+`;
+
+export const RestaurantsScreen = () => (
+  <SafeArea>
+    <SearchContainer>
+      <Searchbar />
+    </SearchContainer>
+
+    <RestaurantListContainer>
+      <RestaurantInfoCard />
+    </RestaurantListContainer>
+  </SafeArea>
+);
